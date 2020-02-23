176b4a27749de1a88e4bf803d534a263a856a540
Marouane Afroukh <m.afroukh@gmail.com>
2020-02-22 20:42:23 -0500

    add search filters

---
+++ app/src/main/java/ca/uqam/ucycle/fragments/FiltersFragment.kt
@@ -0,0 +1,23 @@
+package ca.uqam.ucycle.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ca.uqam.ucycle.R
+
+class FiltersFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_filters, container, false)
+
+
+    companion object {
+        fun newInstance(): FiltersFragment = FiltersFragment()
+    }
+
+
+}
