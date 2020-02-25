9d19d692691d7f72fca94c7d06e6b38ffe70d9e8
Marouane Afroukh <m.afroukh@gmail.com>
2020-02-24 20:12:52 -0500

    fix staggeredLayout

--- app/src/main/java/ca/uqam/ucycle/fragments/ListFragment.kt
+++ app/src/main/java/ca/uqam/ucycle/fragments/ListFragment.kt
@@ -10,10 +10,9 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import ca.uqam.ucycle.R
 import ca.uqam.ucycle.adapters.ListAdapter
+import ca.uqam.ucycle.itemDecoration.SpacesItemDecoration
 import ca.uqam.ucycle.models.Product
 import kotlinx.android.synthetic.main.fragment_list.*
-import java.net.URL
-
 
 class ListFragment : Fragment() {
 
@@ -21,10 +20,10 @@ class ListFragment : Fragment() {
 
     private val products = listOf(
         Product("Persian Cat", "https://i.picsum.photos/id/867/200/300.jpg"),
-        Product("German Fisher", "https://i.picsum.photos/id/868/200/300.jpg"),
-        Product("Old Desk","https://i.picsum.photos/id/869/100/300.jpg"),
+        Product("German Fisher", "https://i.picsum.photos/id/868/200/400.jpg"),
+        Product("Old Desk","https://i.picsum.photos/id/869/200/600.jpg"),
         Product("Dryer", "https://i.picsum.photos/id/870/200/300.jpg"),
-        Product("Winter Tires", "https://i.picsum.photos/id/871/200/300.jpg"),
+        Product("Winter Tires", "https://i.picsum.photos/id/871/200/400.jpg"),
         Product("Vintage Hour", "https://i.picsum.photos/id/872/200/300.jpg"),
         Product("Baseball Cards", "https://i.picsum.photos/id/873/200/300.jpg")
     )
@@ -49,9 +48,12 @@ class ListFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         // RecyclerView node initialized here
         product_list.apply {
-            // set a LinearLayoutManager to handle Android
+            // set a StaggeredGridLayoutManager to handle Android
             // RecyclerView behavior
             layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+
+            val decoration = SpacesItemDecoration(16)
+            addItemDecoration(decoration)
 
             // set the custom adapter to the RecyclerView
             adapter = ListAdapter(products)
@@ -63,9 +65,4 @@ class ListFragment : Fragment() {
         fun newInstance(): ListFragment = ListFragment()
     }
 
-    private fun getImgBitmap(urlText: String): Bitmap {
-        var url = URL(urlText)
-        var bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream())
-        return bmp
-    }
 }
---
+++ app/src/main/java/ca/uqam/ucycle/itemDecoration/SpacesItemDecoration.kt
@@ -0,0 +1,31 @@
+package ca.uqam.ucycle.itemDecoration
+
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ItemDecoration
+
+class SpacesItemDecoration(private val space: Int) : ItemDecoration() {
+
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        outRect.left = space
+        outRect.right = space
+
+        val position = parent.getChildAdapterPosition(view)
+
+        if (position == parent.adapter!!.itemCount - 1) {
+            outRect.bottom = 200
+        }
+
+        if (parent.getChildAdapterPosition(view) == 0)
+            outRect.top = space
+    }
+
+
+}
