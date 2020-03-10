d1df4ccc976a3204a0fd66a7eb6d6b1918eaccfa
Marouane Afroukh <m.afroukh@gmail.com>
2020-03-09 22:16:59 -0400

    pick up image

---
+++ app/src/main/java/ca/uqam/ucycle/data/Product.kt
@@ -0,0 +1,13 @@
+package ca.uqam.ucycle.data
+
+import com.google.firebase.database.Exclude
+
+data class Product(
+
+    @get:Exclude
+    var id: String? = null,
+    var title: String? = null,
+    var description: String? = null,
+    var localisation: String? = null,
+    var urlImage: String? = null
+)
--- app/src/main/java/ca/uqam/ucycle/fragments/PostProductFragment.kt
+++ app/src/main/java/ca/uqam/ucycle/fragments/PostProductFragment.kt
@@ -1,6 +1,11 @@
 package ca.uqam.ucycle.fragments
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -8,17 +13,21 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import ca.uqam.ucycle.R
 import ca.uqam.ucycle.viewModels.CategoriesViewModel
+import com.google.android.material.button.MaterialButton
 
 
 class PostProductFragment : Fragment() {
 
     private lateinit var categoriesViewModel: CategoriesViewModel
     private lateinit var imageProduct: ImageView
+    private lateinit var btnLocalisation : MaterialButton
+    private lateinit var filePath: Uri
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -28,18 +37,19 @@ class PostProductFragment : Fragment() {
 
         var rootView = inflater.inflate(R.layout.fragment_post_product, container, false)
         imageProduct = rootView.findViewById(R.id.post_product_image)
+        btnLocalisation = rootView.findViewById(R.id.btn_localisation)
 
+//        imageProduct.setOnClickListener(this)
+
+        btnLocalisation.setOnClickListener {
+            showFileChooser()
+        }
 
         return rootView
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-
-        imageProduct?.setOnClickListener {
-            Log.i("My App", "Image View Clicked")
-        }
-
 
         val adapter = ArrayAdapter<String>(activity,  android.R.layout.simple_list_item_1)
 
@@ -54,12 +64,33 @@ class PostProductFragment : Fragment() {
             }
         })
 
-        var actv = view?.findViewById<AutoCompleteTextView>(R.id.post_category_autocomplete)
-        actv?.setAdapter(adapter)
+        var autoCompleteCities = view?.findViewById<AutoCompleteTextView>(R.id.post_category_autocomplete)
+        autoCompleteCities?.setAdapter(adapter)
     }
 
 
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data?.data != null) {
+            filePath = data.data!!
+            var bitmap: Bitmap = MediaStore.Images.Media.getBitmap(activity?.contentResolver, filePath)
+            imageProduct.setImageBitmap(bitmap)
+            var layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
+            layoutParams.setMargins(0,0,0,0)
+            imageProduct.layoutParams = layoutParams
+        }
+    }
+
+    private fun showFileChooser() {
+        var intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+    }
+
     companion object {
         fun newInstance(): PostProductFragment = PostProductFragment()
+        private const val PICK_IMAGE_REQUEST = 123
     }
 }
