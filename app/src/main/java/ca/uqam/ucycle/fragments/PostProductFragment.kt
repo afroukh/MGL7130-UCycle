7a560765231943796ef0689b2845120f30c4704b
Marouane Afroukh <m.afroukh@outlook.com>
2020-04-11 18:06:19 -0400

    fix rotation issue

--- app/src/main/java/ca/uqam/ucycle/fragments/PostProductFragment.kt
+++ app/src/main/java/ca/uqam/ucycle/fragments/PostProductFragment.kt
@@ -49,6 +49,11 @@ class PostProductFragment : Fragment() {
     private var categoryId: String = ""
 
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        retainInstance = true
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
